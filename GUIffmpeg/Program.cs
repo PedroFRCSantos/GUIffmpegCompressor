4c1ab1213ef662470cb60738eaa0da68cb064ae3
Pedro Santos FIXO <pedro-santos@hotmail.com>
2016-02-21 14:32:48 +0000

    - First commit.

---
+++ GUIffmpeg/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUIffmpeg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainGui frm = new MainGui();
+            frm.ShowDialog();
+        }
+    }
+}
